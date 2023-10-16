69d65f53c830af91528174885be0819113a65975
Maxime <maxime.marechalmccoy@gmail.com>
2023-10-16 13:05:17 -0700

    exo 1 to 4

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,10 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Update the first store
+@store1 = Store.find_by(id: 1)
+@store1.update(annual_revenue: 311111)
+
+p @store1
+
+@store2 = Store.find_by(id: 2)
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,33 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+# Create 3 more stores , select mens stores, output their names and annual_revenue
+surrey = Store.create(
+  name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+whistler = Store.create(
+  name: "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+yaletown = Store.create(
+  name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+@mens_store = Store.where(mens_apparel: true, womens_apparel: false)
+
+puts "#{@mens_store}"
+
+@mens_store.each {|store| puts "Store name: #{store.name}. Revenue: #{store.annual_revenue}$"}
+
+# @womens_store = Store.where("womens_apparel= true AND annual_revenue < 1000000")
+@womens_store = Store.where(womens_apparel: true).where('annual_revenue > 1000000')
+
+@womens_store.each {|store| puts "Store name: #{store.name}. Revenue: #{store.annual_revenue}$"}
